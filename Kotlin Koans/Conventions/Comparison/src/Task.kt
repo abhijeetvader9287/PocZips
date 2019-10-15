f5aee1e0c4d2f23084b4fd33a6d1e2069dbd4c7f
Abhijeet Vader <a.vader@innopix.solutions>
2019-10-15 18:26:00 +0530

    tste

--- Kotlin Koans/Conventions/Comparison/src/Task.kt
+++ Kotlin Koans/Conventions/Comparison/src/Task.kt
@@ -10,3 +10,7 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparab
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
+fun main() {
+
+    println(  compare(MyDate(2017,2,2),MyDate(2017,3,2)))
+}
--- Kotlin Koans/Conventions/For loop/src/Task.kt
+++ Kotlin Koans/Conventions/For loop/src/Task.kt
@@ -1,4 +1,19 @@
-class DateRange(val start: MyDate, val end: MyDate)
+class DateRange(val start: MyDate, val end: MyDate): Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate>  =DateIterator(this)
+}
+class DateIterator(val dateRange:DateRange):Iterator<MyDate> {
+    var currentDate=dateRange.start
+    override fun hasNext(): Boolean {
+        return  currentDate<=dateRange.end
+          }
+
+    override fun next(): MyDate {
+        val resultDate=currentDate
+        currentDate=currentDate.nextDay()
+        return resultDate
+
+    }
+}
 
 fun iterateOverDateRange(firstDate: MyDate, secondDate: MyDate, handler: (MyDate) -> Unit) {
     for (date in firstDate..secondDate) {
